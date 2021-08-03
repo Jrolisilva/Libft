375e8a174f56a03be5281e21637e3229b97a3f8c
Jair Silva <jroliveira.sa@gmail.com>
2021-08-03 17:33:58 -0300

    Criando Makefile

---
+++ ft_isalnum.c
@@ -0,0 +1,11 @@
+
+int     ftisalnum(int c)
+{
+    if ((c >= '0' && c <= '9')
+                || (c >= 'A' && c <= 'Z')
+                || (c >= 'a' && c <= 'z'))
+        return (1);
+    else
+        return(0);
+}
+
---
+++ libft.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+// Functions part 1
+
+int     ft_isalnum(int c);
+int     ft_isalpha(int c);
+int     ft_isascii(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+int     ft_istolower(int c);
+int     ft_istoupper(int c);
